202e27c55136fff919121c8a063e4c7bad5f3349
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-02-19 14:47:47 +0100

    initial commit

--- js/index.js
+++ js/index.js
@@ -5,9 +5,17 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const newMessage = document.createElement("li");
+  newMessage.textContent = "Another one";
+  newMessage.classList.add("toast-container__message");
+
+  toastContainer.append(newMessage);
+
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = `<li class="toast-container__message">I'm a toast message.</li>
+  `;
   // Exercise: Clear the stack of toast messages
 });
